887575b0885592bdac221d1a2252d8fdc5994109
suzan baert <suzan.baert@outlook.com>
2018-03-17 21:44:38 +0100

    tidy text on sideproject

--- SideProject.R
+++ SideProject.R
@@ -25,13 +25,17 @@ sideproject <- sideproject[!duplicated(sideproject), ]
 
 saveRDS(sideproject, "data/sideproject.RDS")
 
+sideproject <- readRDS("data/sideproject.RDS")
 
 
 # -------
 # visualizing
 # -------
+
 ts_plot(sideproject) +
   labs(title = "Side project tweets", y="number of tweets")
+
+
 
 
 
@@ -40,4 +44,28 @@ ts_plot(sideproject) +
 # -------
 
 
+Encoding(sideproject$text) <- "UTF-8"
+sideproject$text <- iconv(sideproject$text, from="windows-1252", to="UTF-8")
 
+
+regex_remove <- "(@\\S+)|(#?[Mm]y[Ss]ide[Pp]roject)|(side projects?)|(http\\S+)|(&amp)"
+
+
+sideproject %>% 
+  select(status_id, text) %>% 
+  mutate(text = str_replace_all(text, regex_remove, ""),
+         text = str_replace_all(text, "bake", "baking"),
+         text = str_replace_all(text, "knit[^t]", "knitting"),
+         text = str_replace_all(text, "sew[^i]", "sewing"),
+         text = str_replace_all(text, "crochet[^i]", "crocheting"),
+         text = str_replace_all(text, "bike", "biking"),
+         text = str_replace_all(text, "hike", "hiking")) %>% 
+  unnest_tokens(word, text) %>% 
+  filter(!word %in% stop_words$word) %>% 
+  count(word, sort=TRUE) %>% 
+  print(n=30)
+
+
+str_view(sideproject$text, "rochet[^i]", match=TRUE)
+
+#still something wrong. crotchet for instance is given 8 times, but shows as  times...
